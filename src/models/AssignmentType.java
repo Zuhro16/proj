bf7c48d3616a6a5b836267545c20ed2c1d92f2b5
Zuhro16 <YaFatti1611@gmail.com>
2024-10-30 22:13:33 +0500

    Add models

---
+++ src/models/AssignmentType.java
@@ -0,0 +1,7 @@
+package models;
+
+public enum AssignmentType {
+    EXERCISES,
+    PRACTICAL,
+    QUESTION
+}
---
+++ src/models/Student.java
@@ -0,0 +1,9 @@
+package models;
+
+public class Student {
+    private String studentId;
+    private String name;
+    private List<Grade> grades;
+
+    // Конструктор, геттеры и сеттеры
+}
